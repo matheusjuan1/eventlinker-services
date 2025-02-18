6514bfbe93730682b07ca6a90a3270308a53246e
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-02-18 16:17:44 -0300

    Init Project

---
+++ src/main/java/br/com/matheusjuan/eventlinker/EventlinkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusjuan.eventlinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventlinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusjuan/eventlinker/EventlinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusjuan.eventlinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
